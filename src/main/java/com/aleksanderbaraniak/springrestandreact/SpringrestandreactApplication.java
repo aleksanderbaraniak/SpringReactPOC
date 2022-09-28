3bba36006d5a30940ede2bf34601f25ef25155c5
aleksander.baraniak <aleksander.baraniak@accenture.com>
2022-09-28 12:38:23 +0200

    initial commit

---
+++ src/main/java/com/aleksanderbaraniak/springrestandreact/SpringrestandreactApplication.java
@@ -0,0 +1,13 @@
+package com.aleksanderbaraniak.springrestandreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringrestandreactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringrestandreactApplication.class, args);
+	}
+
+}
